9a85ead2d353e017e584368551bed9f322566c8a
HollyJHuber <hollyjhuber@gmail.com>
2017-11-13 15:56:11 -0500

    removed variable for more concise code

--- server.js
+++ server.js
@@ -5,13 +5,10 @@ var http = require ("http");
 let listing = require ("./toDoList");
 
 // get a random item from toDoList
-let item = listing.toDoList[Math.floor((Math.random() * listing.toDoList.length) + 1)];
-
-//console.log(item);
 
 http.createServer(function(request, response) {
   response.writeHead(200, { "Content-type": "text/plain" });
-  response.write(item);
+  response.write(listing.toDoList[Math.floor((Math.random() * listing.toDoList.length) + 1)]);
   response.end();
 }).listen(3000);
  
