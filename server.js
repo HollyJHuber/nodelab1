5d52d6271a5fc0fe83befa3f20ea7875e4f35535
HollyJHuber <hollyjhuber@gmail.com>
2017-11-14 06:38:29 -0500

    added randomItem var

--- server.js
+++ server.js
@@ -3,12 +3,13 @@
 var http = require ("http");
 
 let listing = require ("./toDoList");
+let randomItem = listing.toDoList[Math.floor((Math.random() * listing.toDoList.length) + 1)];
 
 // get a random item from toDoList
 
 http.createServer(function(request, response) {
   response.writeHead(200, { "Content-type": "text/plain" });
-  response.write(listing.toDoList[Math.floor((Math.random() * listing.toDoList.length) + 1)]);
+  response.write(randomItem);
   response.end();
 }).listen(3000);
  
